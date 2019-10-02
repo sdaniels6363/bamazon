50506440aea616af52886087d914478890c1364b
Scott Daniels <sdaniels6363@gmail.com>
2019-10-01 22:57:51 -0400

    added a function to display the inventory, it currently displays as choices for inquirer

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -6,8 +6,37 @@ dotenv.config();
 
 
 // functions
-function displayInventory(inventory){
 
+function displayInventory(inventory) {
+
+  var itemList = [];
+  for (i = 0; i < inventory.length; i++) {
+    var item = inventory[i];
+    var itemObj = {
+      item_id: item.item_id,
+      product_name: item.product_name,
+      department_name: item.department_name,
+      price: item.price,
+      stock_quantity: item.stock_quantity
+    }
+    itemList.push(JSON.stringify(itemObj));
+  }
+
+  inquirer.prompt({
+    type: "list",
+    choices: function () {
+      var choicesArr = [];
+      for (i = 0; i < itemList.length; i++) {
+        var choice = JSON.parse(itemList[i]);
+        choicesArr.push(`Item Name: ${choice.product_name} | Dept Name: ${choice.department_name} | Price: ${choice.price} | Stock: ${choice.stock_quantity}`);
+      }
+      return choicesArr;
+    },
+    message: "What would you like to purchase?",
+    name: "itemSelection"
+  }).then(function (choice) {
+    console.log(`User selected ${choice}`);
+  });
 }
 
 // Creates initial sql connection to the database
@@ -20,7 +49,7 @@ var con = mysql.createConnection({
 });
 
 
-con.connect(function(err){
+con.connect(function (err) {
   if (err) {
     throw err;
   }
@@ -31,7 +60,6 @@ con.connect(function(err){
     if (err) throw err;
     var inventory = result;
     displayInventory(inventory);
-
   });
   con.end(); //close connections
 });
