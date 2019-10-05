179bd7a380c14c144f82a73e4b28290fe578cafa
Scott Daniels <sdaniels6363@gmail.com>
2019-10-05 00:01:19 -0400

    Many changes, adding items to cart works, the total calculation is stilL WIP, input validation added

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -6,10 +6,8 @@ var dotenv = require("dotenv");
 dotenv.config();
 
 // global variables
-var totalCost = parseFloat(0).toFixed(2);
 var cart = [];
-
-
+var totalCost = parseFloat(0).toFixed(2);
 // functions
 
 function displayInventory(inventory) {
@@ -20,21 +18,69 @@ function displayInventory(inventory) {
   }
 }
 
-function decreaseStock(qty) {
-
+function displayCart(cart) {
+  for (i = 0; i < cart.length; i++) {
+    var item = cart[i];
+    console.log(`Item No: ${item.item_id}\nProduct: ${item.product_name}\nQty: ${item.qty}\nSubtotal: $${item.subtotal}`);
+    console.log("-----------------------");
+  }
 }
+
+function calcTotal(cart) {
+
+  for (i = 0; i < cart.length; i++) {
+    var totalCost = parseFloat(totalCost).toFixed(2) + parseFloat(cart[i].subtotal).toFixed(2);
+  }
+  console.log(`Total: $${totalCost}`);
+};
+
+function receipt(cart) {
+  console.log("Your cart contains the following items:");
+  console.log("----------------------------------------");
+  displayCart(cart);
+  console.log("----------------------------------------");
+  calcTotal(cart);
+  console.log("\n\nThank you for shopping at bamazon.\n\n");
+};
+
+function decreaseStock(cart) {
+  // only used after the user has checked out.
+  for (i = 0; i < cart.length; i++) {
+    var item = cart[i]; // write to var for readability
+
+    var itemId = item.item_id;
+    var qtyPurchased = item.qty;
+    // con.query(`UPDATE products SET stock_quantity = "${newQuanity}" WHERE item_id="${itemId}";`, function (err, result) {
+    //   if (err) throw err;
+    // });
+  };
+};
 
 function userOrders() {
   inquirer.prompt([{
     type: "input",
     message: "Enter the Item ID of the Item you'd like to purchase:",
-    name: "itemSelection"
+    name: "itemSelection",
+    validate: function validItemNo(val){
+      var num = parseInt(val)
+      if (num >= 1 && num <= inventory.length){
+        return true;
+      } else {
+        console.log("\n\nPlease enter a valid item number.\n")
+      }
+    }
   }, {
     type: "input",
     message: "How many units would you like to purchase?:",
-    name: "amountDesired"
-  }
-  ]).then(function (response) {
+    name: "amountDesired",
+    validate: function nonNegative(qty) {
+      if (qty < 0) {
+        console.log("\n\nYou cannot enter a negative quantity.\n");
+        return;
+      }
+      return true;
+    }
+  }]).then(function (response) {
     var chosenItem = response.itemSelection;
     var qty = response.amountDesired;
     con.query(`SELECT * FROM products WHERE item_id="${chosenItem}";`, function (err, result) {
@@ -49,65 +95,97 @@ function userOrders() {
         // If the user orders an item that is not in stock, they are notified.  Also applies to if they ordered 51 items, and only 50 were in stock.
         // Program is re-run if this is the case.
         console.log("\n\nThere are not enough items in stock to fulfill your order.\n\n");
-        runProgram();
+
+        inquirer.prompt({
+          message: "Would you like to continue shopping?",
+          type: "confirm",
+          name: "keepShopping"
+        }).then(function (response) {
+
+          if (response.keepShopping) {
+            runProgram();
+          } else {
+            console.log("Thank you for shopping at bamazon.");
+            con.end();
+          }
+        });
       } else {
         var price = parseFloat(item.price).toFixed(2);
 
         var subtotal = parseFloat(price * qty).toFixed(2);
-        console.log(`Your total cost for ${qty} ${item.product_name}(s) is: $${totalCost}\n`);
+        console.log(`Your total cost for ${qty} ${item.product_name}(s) is: $${subtotal}\n`);
 
         inquirer.prompt([{
           type: "confirm",
           message: "Would you like to add these items to your cart?",
           name: "addToCart"
-        },{
+        }, {
           type: "confirm",
           message: "Are you finished shopping?",
           name: "checkout"
-        }]).then(function(response){
+        }]).then(function (response) {
 
-          if (response.addToCart && response.checkout){
+          if (response.addToCart && response.checkout) {
+
             // if the user is adding to cart and ready to checkout
-            
+            var newItem = {
+              item_id: item.item_id,
+              product_name: item.product_name,
+              qty: qty,
+              subtotal: parseFloat(qty * item.price).toFixed(2)
+            };
             // display total cost to customer, and list of items being purchased then exit connection
+            console.log(cart);
+            cart.push(newItem);
+            receipt(cart);
+            decreaseStock(cart);
             con.end();
-          } else if (response.addToCart && !response.checkout){
+
+          } else if (response.addToCart && !response.checkout) {
+
             // if user is adding to cart, but not ready to checkout
             var newItem = {
+              item_id: item.item_id,
               product_name: item.product_name,
-              quantity_desired: qty
-            }
-            var transaction = parseFloat(totalCost + subtotal);
-            // push item+qty to cart, add subotal to bill, and redisplay inventory
+              qty: qty,
+              subtotal: parseFloat(qty * item.price).toFixed(2)
+            };
+            // add item to cart
             cart.push(newItem);
-            runProgram();      
-          } else if (!response.addToCart && !response.checkout){
+            runProgram();
+
+          } else if (!response.addToCart && response.checkout) {
+
             // user is not adding to cart, but is ready to checkout
-
-            // display current items in cart, and existing total.
-            console.log("Your cart contains the following items:");
-            console.log("---------------------------------------");
-            displayCart();
-            console.log("---------------------------------------");
-            console.log(`Your total is ${totalCost}`);
-
+            if (cart.length === 0) {
+              // if user didn't want anything.
+              console.log("\n\nThank you for shopping at bamazon.\n\n");
+              con.end();
+            } else {
+              // display current items in cart, and existing total.
+              receipt(cart);
+              decreaseStock(cart);
+              con.end();
+            }
           } else {
             // user is not adding to cart and not ready to checkout.
+
+
             runProgram();
           }
 
-        });
-
 
         }
-    })
+        );
+      }
+    });
   });
 }
 
 function runProgram() {
   con.query("SELECT * FROM products;", function (err, result) {
     if (err) throw err;
-    var inventory = result;
+    inventory = result;
     displayInventory(inventory);
     userOrders();
   });
