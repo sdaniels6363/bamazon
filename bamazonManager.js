cc309a9c3f28e4e6b3ca124732672d74b85727ba
Scott Daniels <sdaniels6363@gmail.com>
2019-10-07 16:43:44 -0400

    low inventory option works, displays a list of the items that are low

--- bamazonManager.js
+++ bamazonManager.js
@@ -4,38 +4,75 @@ var dotenv = require('dotenv');
 
 dotenv.config();
 
+function keepRunning() {
+  inquirer.prompt({
+    message: "Would you like to continue?",
+    type: "confirm",
+    name: "confirm"
+  }).then(function (response) {
+    if (response.confirm) {
+      runProgram();
+    } else {
+      return false;
+    }
+  });
+}
+
 function displayInventory() {
   con.query("SELECT * FROM products;", function (err, result) {
     if (err) throw err;
+    inventory = [];
 
     // Print every item from the inventory.
     for (i = 0; i < result.length; i++) {
       var item = result[i];
-      console.log(`${item.item_id}) ${item.product_name} | Dept: ${item.department_name} | Price: ${item.price} | Stock: ${item.stock_quantity}`)
+      console.log(`${item.item_id}) ${item.product_name} | Dept: ${item.department_name} | Price: ${item.price} | Stock: ${item.stock_quantity}`);
+      inventory.push(item.item_id);
     }
     console.log("\n\n")
   });
-
+  keepRunning();
 }
 
 function viewLowInventory() {
   con.query(`SELECT * FROM products WHERE stock_quantity<="5";`, function (err, response) {
     if (err) throw err;
+    if (response) {
+      console.log((response));
+      console.log(`\nThe following Items are low on inventory:`)
+      if (response.length > 1) {
+        for (i = 0; i < response.length; i++) {
+          var item = response[i];
+          var itemId = item.item_id;
+          var prodName = item.product_name;
+          var deptName = item.department_name;
+          var qty = item.stock_quantity;
+          console.log(`${itemId}) Product Name: ${prodName} | Dept Name: ${deptName} | Qty: ${qty}`)
+        }
+      } else {
+        var item = response[0];
+        var itemId = item.item_id;
+        var prodName = item.product_name;
+        var deptName = item.department_name;
+        var qty = item.stock_quantity;
+        console.log(`${itemId}) Product Name: ${prodName} | Dept Name: ${deptName} | Qty: ${qty}\n`)
+      }
 
-    console.log(response);
+    } else {
+      console.log("No inventory is dangerously low.")
+    }
   });
+  keepRunning();
 };
 
 function increaseStock() {
-  // add inquirer prompts to prompt for item_id, and new desired amount
-  // add query to poll the existing stock of the item id
   inquirer.prompt([{
     message: "What Item ID would you like add stock for?",
     type: "input",
     name: "itemId",
-    validate: function (val) {
-      var num = parseInt(val)
-      if (num >= 1 && num <= inventory.length) {
+    validate: function (itemId) {
+      var num = parseInt(itemId)
+      if (inventory.contains(num)) {
         return true;
       } else {
         console.log("\n\nPlease enter a valid item number.\n")
@@ -57,14 +94,18 @@ function increaseStock() {
     var stockToAdd = response.qtyToAdd;
     var itemId = response.itemId;
     // this query determines current stock
-    con.query(`SELECT stock_quantity FROM products WHERE item_id="${itemId}";`, function(err,response){
+    con.query(`SELECT stock_quantity FROM products WHERE item_id="${itemId}";`, function (err, response) {
       if (err) throw err;
-      currentStock = response;
+      currentStock = response[0];
     });
     // this query updates the stock.
     var newStock = currentStock + stockToAdd;
-    con.query(`UPDATE products SET stock_quantity = "${newStock}" WHERE item_id="${itemId}";`, function (err) {
+    con.query(`UPDATE products SET stock_quantity = "${newStock}" WHERE item_id="${itemId}";`, function (err, response) {
       if (err) throw err;
+      // print response
+      console.log(response);
+      // start over program
+      keepRunning();
     });
   });
 };
@@ -108,7 +149,7 @@ function addNewProduct() {
       if (err) throw err;
       console.log(response);
     });
-
+    keepRunning();
   });
 }
 
@@ -126,14 +167,19 @@ function runProgram() {
     switch (choice) {
       case "View Products for Sale":
         displayInventory();
+        break;
+
       case "View Low Inventory":
         viewLowInventory();
+        break;
+
       case "Add to Inventory":
         displayInventory();
         increaseStock();
+        break;
+
       case "Add New Product":
         addNewProduct();
-        runProgram();
         break;
 
     }
